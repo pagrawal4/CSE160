7954c4c2e97ecc55ac7cb14ac604c27dec8701d1
Pranjal Agrawal <pagrawa4@ucsc.edu>
2025-02-02 07:53:39 -0800

    Added arms and sliders for arms

--- ASG2/src/asg2.js
+++ ASG2/src/asg2.js
@@ -36,6 +36,10 @@ let g_upperRightLegAngle=0;
 let g_upperLeftLegAngle=0;
 let g_lowerRightLegAngle=0;
 let g_lowerLeftLegAngle=0;
+let g_upperRightArmAngle=-45;
+let g_upperLeftArmAngle=-45;
+let g_lowerRightArmAngle=90;
+let g_lowerLeftArmAngle=90;
 let g_rightFeetAngle=0;
 let g_leftFeetAngle=0;
 let g_moveXPosition=0;
@@ -120,6 +124,16 @@ function addActionsForHtmlUI() {
   // Feet slider events
   document.getElementById("leftFeetSlide").addEventListener("mousemove", function() { g_leftFeetAngle = this.value; renderScene();});
 
+  // Upper arm slider events
+  document.getElementById("upperRightArmSlide").addEventListener("mousemove", function() { g_upperRightArmAngle = this.value; renderScene();});
+  // Upper arm slider events
+  document.getElementById("upperLeftArmSlide").addEventListener("mousemove", function() { g_upperLeftArmAngle = this.value; renderScene();});
+
+  // Lower arm slider events
+  document.getElementById("lowerRightArmSlide").addEventListener("mousemove", function() { g_lowerRightArmAngle = this.value; renderScene();});
+  // Lower arm slider events
+  document.getElementById("lowerLeftArmSlide").addEventListener("mousemove", function() { g_lowerLeftArmAngle = this.value; renderScene();});
+
   // Camera moveX slider events
   document.getElementById("moveXSlide").addEventListener("mousemove", function() { g_moveXPosition = this.value; renderScene();});
   // Camera moveY slider events
@@ -174,7 +188,7 @@ function tick() {
   // Save the current time
   g_seconds = performance.now()/1000.0 - g_startTime;
   // Print some debug information so we know that it is being called
-  console.log(g_seconds);
+  //console.log(g_seconds);
 
   /*
   if (stateChanged()) {
@@ -200,6 +214,10 @@ function resetState() {
   g_upperLeftLegAngle = 0;
   g_lowerRightLegAngle = 0;
   g_lowerLeftLegAngle = 0;
+  g_upperRightArmAngle = -45;
+  g_upperLeftArmAngle = -45;
+  g_lowerRightArmAngle = 90;
+  g_lowerLeftArmAngle = 90;
   g_rightFeetAngle = 0;
   g_leftFeetAngle = 0;
   g_moveXPosition = 0;
@@ -216,6 +234,10 @@ function printState() {
   console.log("g_leftFeetAngle " + g_leftFeetAngle);
   console.log("g_moveXPosition " + g_moveXPosition);
   console.log("g_moveYPosition " + g_moveYPosition);
+  console.log("g_upperRightArmAngle " + g_upperRightArmAngle);
+  console.log("g_upperLeftArmAngle " + g_upperLeftArmAngle);
+  console.log("g_lowerRightArmAngle " + g_lowerRightArmAngle);
+  console.log("g_lowerLeftArmAngle " + g_lowerLeftArmAngle);
 }
 
 
@@ -223,7 +245,7 @@ let g_tickNum = -1;
 function updateAnimationAngles() {
 
   if (g_tickNum == -1) {
-    resetState();
+    //resetState();
     g_moveXPosition += 60;
     g_moveYPosition += 3;
   }
@@ -303,7 +325,8 @@ function renderScene() {
   // Clear canvas
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  // V IMP: The transformation that is specified first is the last transformation on the points!!!
+  // V IMP: The transformation that is specified first is the last transformation 
+  // on the points!!!
 
   // FLOOR
 
@@ -313,95 +336,113 @@ function renderScene() {
 
   // BODY
 
-  let bodyM = new Matrix4();
-  bodyM.translate(g_moveXPosition/100, g_moveYPosition/100, 0);// -0.1, -0.25, -0.15);
-
-  // Set reference for upper left and right legs
-  let rightUpperLegM = new Matrix4(bodyM).translate(0, -0.2, -0.1);
-  let leftUpperLegM = new Matrix4(bodyM).translate(0, -0.2, 0.1);
+  let baseBodyM = new Matrix4();
+  baseBodyM.translate(g_moveXPosition/100, g_moveYPosition/100, 0);// -0.1, -0.25, -0.15);
 
   // Scale and draw body
-  bodyM.scale(0.3, 0.5, 0.3);
+  let bodyM = new Matrix4(baseBodyM).scale(0.3, 0.5, 0.3);
   bodyC = [1, 0, 0, 1];
   drawCube(bodyM, bodyC);
 
   // HEAD and HAT
 
   // Set reference for head
-  let headM = new Matrix4(bodyM).translate(0, 0.85, 0);
+  let baseHeadM = new Matrix4(baseBodyM).translate(0, 0.425, 0);
+
   // Scale and draw head
-  headM.scale(1, 0.5, 0.9);
+  let headM = new Matrix4(baseHeadM);
+  headM.scale(0.3, 0.25, 0.27);
   headC = [1, 1, 1, 1];
   drawCube(headM, headC);
 
   // Set reference for neck
-  let neckM = new Matrix4(bodyM).translate(0, 0.75, 0);
+  let neckM = new Matrix4(baseBodyM).translate(0, 0.375, 0);
   // Scale and draw neck
-  neckM.scale(0.6, 0.5, 0.6);
+  neckM.scale(0.18, 0.35, 0.18);
   neckC = [1, 1, 0, 1];
   drawCube(neckM, neckC);
 
   // Set reference for right ear
-  let rightEarM = new Matrix4(bodyM).translate(0, 0.85, -0.5);
+  let rightEarM = new Matrix4(baseHeadM).translate(0, 0, -0.15);
   // Scale and draw rightEar
-  rightEarM.scale(0.15, 0.25, 0.2);
+  rightEarM.scale(0.045, 0.125, 0.06);
   rightEarC = [1, 0.9, 0.1, 1];
   drawCube(rightEarM, rightEarC);
 
   // Set reference for left ear
-  let leftEarM = new Matrix4(bodyM).translate(0, 0.85, 0.5);
+  let leftEarM = new Matrix4(baseHeadM).translate(0, 0, 0.15);
   // Scale and draw leftEar
-  leftEarM.scale(0.15, 0.25, 0.2);
+  leftEarM.scale(0.045, 0.125, 0.06);
   leftEarC = [1, 0.9, 0.1, 1];
   drawCube(leftEarM, leftEarC);
 
   // Set reference for nose
-  let noseM = new Matrix4(bodyM).translate(0.5, 0.85, 0);
+  let noseM = new Matrix4(baseHeadM).translate(0.15, 0, 0);
   noseM.rotate(25, 0, 0, 1);
   // Scale and draw nose
-  noseM.scale(0.15, 0.25, 0.25);
+  noseM.scale(0.045, 0.125, 0.075);
   noseC = [1, 0.8, 0, 1];
   drawCube(noseM, noseC);
 
   // HAT
 
   // Set reference for brim of hat
-  let brimM = new Matrix4(headM).translate(0, 0.55, 0);
+  let brimM = new Matrix4(baseHeadM).translate(0, 0.15, 0);
   // Scale and draw brim
-  brimM.scale(1.8, 0.1, 1.8);
+  brimM.scale(0.54, 0.05, 0.54);
   brimC = [1, 0, 0, 1];
   drawCube(brimM, brimC);
 
   // Set reference for hat
-  let hatM = new Matrix4(headM).translate(0, 0.9, 0);
+  let hatM = new Matrix4(baseHeadM).translate(0, 0.275, 0);
   // Scale and draw hat
-  hatM.scale(1.1, 0.7, 1.1);
+  hatM.scale(0.3, 0.20, 0.3);
   hatC = [1, 0, 0.5, 1];
   drawCube(hatM, hatC);
 
-  // LEGS
+  // RIGHT LEG
 
+  // Set reference for upper left and right legs
+  let rightUpperLegM = new Matrix4(baseBodyM).translate(0, -0.2, -0.1);
   // Rotate and translate right leg
   let rightUpperLegC = [1, 1, 0, 1];
   rightUpperLegM.rotate( g_upperRightLegAngle, 0, 0, 1);
   rightUpperLegM.translate(0.0, -0.1, 0.01);
 
-  // Rotate and translate left leg
-  let leftUpperLegC = [1*0.9, 1*0.9, 0, 1];
-  leftUpperLegM.rotate( g_upperLeftLegAngle, 0, 0, 1);
-  leftUpperLegM.translate(0.0, -0.1, -0.01);
-
   // Set reference for lower left and right legs
   let rightLowerLegM = new Matrix4(rightUpperLegM).translate(0.06, -0.25, 0);
-  let leftLowerLegM = new Matrix4(leftUpperLegM).translate(0.06, -0.25, 0);
-
   rightUpperLegM.scale(0.12, 0.5, 0.11);
-  leftUpperLegM.scale(0.12, 0.5, 0.11);
 
   // Scale and translate left leg
   let rightLowerLegC = [1, 1, 0, 1];
   rightLowerLegM.rotate( g_lowerRightLegAngle, 0, 0, 1);
   rightLowerLegM.translate(-0.06, -0.2, 0);
+
+  drawCube(rightUpperLegM, rightUpperLegC);
+
+  // Set reference for lower left and right legs
+  let rightFeetM = new Matrix4(rightLowerLegM).translate(-0.06, -0.2, 0);
+  rightLowerLegM.scale(0.12, 0.4, 0.10);
+  drawCube(rightLowerLegM, rightLowerLegC);
+
+  // Scale and translate left feet
+  let rightFeetC = [0, 0, 1, 1];
+  rightFeetM.rotate( g_rightFeetAngle, 0, 0, 1);
+  rightFeetM.translate(0.1, 0.05, 0);
+
+  rightFeetM.scale(0.25, 0.11, 0.101);
+  drawCube(rightFeetM, rightFeetC);
+
+  // LEFT LEG
+
+  let leftUpperLegM = new Matrix4(baseBodyM).translate(0, -0.2, 0.1);
+  // Rotate and translate left leg
+  let leftUpperLegC = [1*0.9, 1*0.9, 0, 1];
+  leftUpperLegM.rotate( g_upperLeftLegAngle, 0, 0, 1);
+  leftUpperLegM.translate(0.0, -0.1, -0.01);
+
+  let leftLowerLegM = new Matrix4(leftUpperLegM).translate(0.06, -0.25, 0);
+  leftUpperLegM.scale(0.12, 0.5, 0.11);
 
   // Scale and translate right leg
   let leftLowerLegC = [1, 1, 0, 1];
@@ -409,43 +450,67 @@ function renderScene() {
   leftLowerLegM.translate(-0.06, -0.2, 0);
 
   drawCube(leftUpperLegM, leftUpperLegC);
-  drawCube(rightUpperLegM, rightUpperLegC);
 
-  // Set reference for lower left and right legs
-  let rightFeetM = new Matrix4(rightLowerLegM).translate(-0.06, -0.2, 0);
   let leftFeetM = new Matrix4(leftLowerLegM).translate(-0.06, -0.2, 0);
 
-  rightLowerLegM.scale(0.12, 0.4, 0.10);
   leftLowerLegM.scale(0.12, 0.4, 0.10);
-
-  drawCube(leftLowerLegM);
-  drawCube(rightLowerLegM);
-
-  // Scale and translate left feet
-  let rightFeetC = [0, 0, 1, 1];
-  rightFeetM.rotate( g_rightFeetAngle, 0, 0, 1);
-  rightFeetM.translate(0.1, 0.05, 0);
+  drawCube(leftLowerLegM, leftLowerLegC);
 
   // Scale and translate right feet
   let leftFeetC = [0, 0, 1, 1];
   leftFeetM.rotate( g_leftFeetAngle, 0, 0, 1);
   leftFeetM.translate(0.1, 0.05, 0);
 
-  rightFeetM.scale(0.25, 0.11, 0.101);
   leftFeetM.scale(0.25, 0.11, 0.101);
-
   drawCube(leftFeetM, leftFeetC);
-  drawCube(rightFeetM, rightFeetC);
 
-  /***
-  // Draw left arm lower
-  leftArmLowerM.translate(0.0, 0.65, 0.0);
-  leftArmLowerM.rotate(g_moveXPosition, 0, 0, 1);
-  leftArmLowerM.scale(0.3, 0.3, 0.3);
-  leftArmLowerM.translate(-0.5, 0, -0.001); // Last value is to remove z-fight flicker
-  leftArmLowerC = [1, 0, 1, 1];
-  drawCube(leftArmLowerM, leftArmLowerC);
-***/
+  // ARMS
+
+  // RIGHT ARM
+
+  // Set reference for upper left and right arms
+  let rightUpperArmM = new Matrix4(baseBodyM).translate(0, 0.2, -0.21);
+  // Rotate and translate right arm
+  let rightUpperArmC = [0, 0, 1, 1];
+  rightUpperArmM.rotate( g_upperRightArmAngle, 0, 0, 1);
+  rightUpperArmM.translate(0.0, -0.16, 0.01);
+
+  // Set reference for lower left and right arms
+  let rightLowerArmM = new Matrix4(rightUpperArmM).translate(-0.06, -0.16, 0);
+  rightUpperArmM.scale(0.12, 0.32, 0.11);
+
+  // Scale and translate left arm
+  let rightLowerArmC = [0, 0, 1, 1];
+  rightLowerArmM.rotate( g_lowerRightArmAngle, 0, 0, 1);
+  rightLowerArmM.translate(0.06, -0.16, 0);
+
+  drawCube(rightUpperArmM, rightUpperArmC);
+
+  rightLowerArmM.scale(0.12, 0.32, 0.10);
+  drawCube(rightLowerArmM, rightLowerArmC);
+
+  // LEFT ARM
+
+  // Set reference for upper left and left arms
+  let leftUpperArmM = new Matrix4(baseBodyM).translate(0, 0.2, 0.21);
+  // Rotate and translate left arm
+  let leftUpperArmC = [0, 0, 1, 1];
+  leftUpperArmM.rotate( g_upperLeftArmAngle, 0, 0, 1);
+  leftUpperArmM.translate(0.0, -0.16, -0.01);
+
+  // Set reference for lower left and left arms
+  let leftLowerArmM = new Matrix4(leftUpperArmM).translate(-0.06, -0.16, 0);
+  leftUpperArmM.scale(0.12, 0.32, 0.11);
+
+  // Scale and translate left arm
+  let leftLowerArmC = [0, 0, 1, 1];
+  leftLowerArmM.rotate( g_lowerLeftArmAngle, 0, 0, 1);
+  leftLowerArmM.translate(0.06, -0.16, 0);
+
+  drawCube(leftUpperArmM, leftUpperArmC);
+
+  leftLowerArmM.scale(0.12, 0.32, 0.10);
+  drawCube(leftLowerArmM, leftLowerArmC);
 
   // Use the start and current time to record duration (in ms)
   var duration = performance.now() - startTime;
