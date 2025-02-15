aff8cbb5507ec8e21a2446479b6156834b2de603
Pranjal Agrawal <pagrawa4@ucsc.edu>
2025-02-15 07:16:23 -0800

    separate x,y,z sizes of map

--- ASG3/src/map.js
+++ ASG3/src/map.js
@@ -1,33 +1,34 @@
 
 class Map {
     constructor() {
-        this.size = 10; // map is size x size
+        this.size = [10, 4, 10]; // map is size[0]x[size[2] and height is size[1]
         this.objectsInPosition = [];
         this.map = [
-            [1,1,1,1,1,1,1,1,1,1],
+            [1,0,1,0,1,1,1,1,1,1],
+            [0,0,0,0,0,0,0,0,0,1],
+            [0,0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0,1],
             [0,0,0,0,0,0,0,0,0,1],
+            [0,0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0,1],
             [0,0,0,0,0,0,0,0,0,1],
-            [0,0,0,0,0,0,0,0,0,1],
-            [0,0,0,0,0,0,0,0,0,1],
-            [0,0,0,0,0,0,0,0,0,1],
-            [0,0,0,0,0,0,0,0,0,1],
-            [1,1,1,1,1,1,1,1,1,1],
+            [0,0,0,0,0,0,0,0,0,0],
+            [1,1,1,0,1,1,1,0,1,1],
         ]
         this.positionObjects();
     }
 
     // Do as much work upfront as possible to speed up rendering
     positionObjects() {
-        let n = this.size;
-        for (let x = 0; x < n; x++) {
+        let nx = this.size[0];
+        let nz = this.size[2];
+        for (let x = 0; x < nx; x++) {
             let row = [];
-            for (let y = 0; y < n; y++) {
-                if (this.map[x][y]==1) {
+            for (let z = 0; z < nz; z++) {
+                if (this.map[x][z]==1) {
                     let c = new Cube();
-                    c.textureNum = -1;
-                    c.matrix.translate(x-n/2,0.5,y-n/2);
+                    c.textureNum = 0;
+                    c.matrix.translate(x-nx/2,0.5,z-nz/2);
                     row.push(c);
                 }
             }
@@ -36,11 +37,12 @@ class Map {
     }
 
     render() {
-        let n = this.size;
-        for (let x = 0; x < n; x++) {
+        let nx = this.size[0];
+        let nz = this.size[2];
+        for (let x = 0; x < nx; x++) {
             let row = this.objectsInPosition[x];
-            for (let y = 0; y < n; y++) {
-                let c = row[y];
+            for (let z = 0; z < nz; z++) {
+                let c = row[z];
                 if (c != null) {
                     c.render();
                 }
