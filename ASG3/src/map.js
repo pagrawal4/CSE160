a84c83314265d9d4626903dbe457e592657706c3
Pranjal Agrawal <pagrawa4@ucsc.edu>
2025-02-15 06:41:06 -0800

    Positions blocks on surface

--- ASG3/src/map.js
+++ ASG3/src/map.js
@@ -1,10 +1,11 @@
 
 class Map {
     constructor() {
-        this.size = 2; // map is size x size
+        this.size = 10; // map is size x size
         this.objectsInPosition = [];
         this.map = [
             [1,1,1,1,1,1,1,1,1,1],
+            [0,0,0,0,0,0,0,0,0,1],
             [0,0,0,0,0,0,0,0,0,1],
             [0,0,0,0,0,0,0,0,0,1],
             [0,0,0,0,0,0,0,0,0,1],
@@ -19,13 +20,14 @@ class Map {
 
     // Do as much work upfront as possible to speed up rendering
     positionObjects() {
-        for (let x = 0; x < this.size; x++) {
+        let n = this.size;
+        for (let x = 0; x < n; x++) {
             let row = [];
-            for (let y = 0; y < this.size; y++) {
+            for (let y = 0; y < n; y++) {
                 if (this.map[x][y]==1) {
                     let c = new Cube();
                     c.textureNum = -1;
-                    c.matrix.translate(x,y+0.5,0);
+                    c.matrix.translate(x-n/2,0.5,y-n/2);
                     row.push(c);
                 }
             }
@@ -34,9 +36,10 @@ class Map {
     }
 
     render() {
-        for (let x = 0; x < this.size; x++) {
+        let n = this.size;
+        for (let x = 0; x < n; x++) {
             let row = this.objectsInPosition[x];
-            for (let y = 0; y < this.size; y++) {
+            for (let y = 0; y < n; y++) {
                 let c = row[y];
                 if (c != null) {
                     c.render();
