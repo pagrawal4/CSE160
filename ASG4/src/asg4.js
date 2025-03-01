16740b42e0a573ecbbac1b7c4bf42426068a56a9
Pranjal Agrawal <pagrawa4@ucsc.edu>
2025-03-01 01:36:15 -0800

    Add ambient lighting

--- ASG4/src/asg4.js
+++ ASG4/src/asg4.js
@@ -73,21 +73,25 @@ var FSHADER_SOURCE = `
     }
     vec3 lightVector = u_lightPos - vec3(v_VertPos);
     float r = length(lightVector);
-    /*
-    if (r<1.0) {
-      gl_FragColor = vec4(1,0,0,1);
-    } else if (r<2.0){
-     gl_FragColor = vec4(0,1,0,1);
-    }
-    */
+
+    // Red/Green Distance Visualization
+    //if (r<1.0) {
+    //  gl_FragColor = vec4(1,0,0,1);
+    //} else if (r<2.0){
+    // gl_FragColor = vec4(0,1,0,1);
+    //}
+
+    // Light Falloff Visualization 1/r^2
     //gl_FragColor = vec4(vec3(gl_FragColor)/(r*r),1);
 
     // N dot L
     vec3 L = normalize(lightVector);
     vec3 N = normalize(v_Normal);
     float nDotL = max(dot(N,L), 0.0);
-    gl_FragColor = gl_FragColor * nDotL;
-    gl_FragColor.a = 1.0; // alpha
+
+    vec3 diffuse = vec3(gl_FragColor) * nDotL;
+    vec3 ambient = vec3(gl_FragColor) * 0.3;
+    gl_FragColor = vec4(diffuse+ambient, 1.0);
   }`
 
 // Global Variables
