6a20443133e2b9f24806a98a7be01aa6cc12b1b9
Pranjal Agrawal <pagrawa4@ucsc.edu>
2025-03-02 18:29:50 -0800

    Added normals for tiny homes

--- ASG4/src/home.js
+++ ASG4/src/home.js
@@ -4,11 +4,14 @@ class Home {
       // this.position = [0.0, 0.0, 0.0];
       this.color = [1.0, 1.0, 1.0, 1.0];
       this.matrix = new Matrix4(); // default identity
+      this.normalMatrix = new Matrix4();
       this.textureNum = 1;
       this.texColorWeight = 1.0;
       this.hasShinySurface = false;
 
       this.vertexBuffer = null;
+      this.uvBuffer = null;
+      this.normalBuffer = null;
 
       this.vertices = new Float32Array([
           //FRONT
@@ -65,10 +68,40 @@ class Home {
           0.50,0.50,  0.50,0.00,  1.00,0.00,
           0.50,0.50,  1.00,0.00,  1.00,0.50,
         ]);
+
+        this.normals = new Float32Array([
+            //FRONT
+            0,0,1,  0,0,1,  0,0,1,
+            0,0,1,  0,0,1,  0,0,1,
+            //LEFT
+            -1,0,0,  -1,0,0,  -1,0,0,
+            -1,0,0,  -1,0,0,  -1,0,0,
+            //RIGHT
+            1,0,0,  1,0,0,  1,0,0,
+            1,0,0,  1,0,0,  1,0,0,
+            //BACK
+            0,0,-1,  0,0,-1,  0,0,-1,
+            0,0,-1,  0,0,-1,  0,0,-1,
+            //BOTTOM
+            0,-1,0,  0,-1,0,  0,-1,0,
+            0,-1,0,  0,-1,0,  0,-1,0,
+            // FRONT ROOF
+            0,0,1,  0,0,1,  0,0,1,
+            // BACK ROOF
+            0,0,-1,  0,0,-1,  0,0,-1,
+            // RIGHT ROOF
+            0.81373,0.58123,0,   0.81373,0.58123,0,   0.81373,0.58123,0,
+            0.81373,0.58123,0,   0.81373,0.58123,0,   0.81373,0.58123,0,
+            // LEFT ROOF
+            -0.81373,0.58123,0,  -0.81373,0.58123,0,  -0.81373,0.58123,0,
+            -0.81373,0.58123,0,  -0.81373,0.58123,0,  -0.81373,0.58123,0,
+          ]);
   }
 
   render() {
       var rgba = this.color;
+
+      this.normalMatrix.setInverseOf(this.matrix).transpose();
 
       // Set the u_TextureSelect to textureNum
       if (g_normalsOn) {
@@ -87,16 +120,18 @@ class Home {
       // Pass the matrix to u_ModelMatrix attribute
       gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
+      gl.uniformMatrix4fv(u_NormalMatrix, false, this.normalMatrix.elements);
+
      // Create a buffer object
-      if (this.buffer == null) {
-          this.buffer = gl.createBuffer();
-          if (!this.buffer) {
+      if (this.vertexBuffer == null) {
+          this.vertexBuffer = gl.createBuffer();
+          if (!this.vertexBuffer) {
               console.log('Failed to create the buffer object');
               return -1;
           }
       }
       // Bind the buffer object to target
-      gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
       // Write date into the buffer object
       // DYNAMIC gives better performance, but changing back to STATIC
       // because getting "Vertex buffer is not big enough for the draw call"
@@ -110,19 +145,32 @@ class Home {
       gl.enableVertexAttribArray(a_Position);
 
       // Create a buffer object
-      if (this.uvbuffer == null) {
-          this.uvbuffer = gl.createBuffer();
-          if (!this.uvbuffer) {
+      if (this.uvBuffer == null) {
+          this.uvBuffer = gl.createBuffer();
+          if (!this.uvBuffer) {
               console.log('Failed to create the buffer object');
               return -1;
           }
       }
-      gl.bindBuffer(gl.ARRAY_BUFFER, this.uvbuffer);
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.uvBuffer);
       gl.bufferData(gl.ARRAY_BUFFER, this.uv, gl.DYNAMIC_DRAW);
       gl.vertexAttribPointer(a_UV, 2, gl.FLOAT, false, 0, 0);
       gl.enableVertexAttribArray(a_UV);
 
-      gl.drawArrays(gl.TRIANGLES, 0, 48);
+      // Create a buffer object
+      if (this.normalBuffer == null) {
+        this.normalBuffer = gl.createBuffer();
+        if (!this.normalBuffer) {
+            console.log('Failed to create the buffer object');
+            return -1;
+        }
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.normalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, this.normals, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(a_Normal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Normal);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 48);
   }
 }
 
