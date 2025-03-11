b5f34f050a209af1586e56bf0e6f16efcf617c43
Pranjal Agrawal <pagrawa4@ucsc.edu>
2025-03-11 05:38:25 -0700

    Add outside wall and resize other pieces

--- ASG5/src/fort.js
+++ ASG5/src/fort.js
@@ -27,18 +27,36 @@ function createHideout(){
   return hideout;
 }
 
-function createWall() {
+function createInsideWall() {
   const wall = new THREE.Group();
 
-  const baseGeometry = new THREE.BoxGeometry(4*x, 3*x, 0.5*x);
+  const baseGeometry = new THREE.BoxGeometry(8*x, 3*x, 0.5*x);
   const baseMaterial = new THREE.MeshStandardMaterial({color: 0xff00ff});
   const base = new THREE.Mesh(baseGeometry, baseMaterial);
   wall.add(base);
 
-  for (let i =-1; i <= 2; i++) {
+  for (let i =-3; i <= 3; i++) {
     const hideout = createHideout();
     hideout.position.x = i*x;
     hideout.position.y = 1.75*x;
+    wall.add(hideout);
+  }
+
+  return wall;
+}
+
+function createOutsideWall() {
+  const wall = new THREE.Group();
+
+  const baseGeometry = new THREE.BoxGeometry(19.5*x, 2.5*x, 0.5*x);
+  const baseMaterial = new THREE.MeshStandardMaterial({color: 0xff00ff});
+  const base = new THREE.Mesh(baseGeometry, baseMaterial);
+  wall.add(base);
+
+  for (let i =-9; i <= 9; i++) {
+    const hideout = createHideout();
+    hideout.position.x = i*x;
+    hideout.position.y = 1.3*x;
     wall.add(hideout);
   }
 
@@ -65,50 +83,68 @@ function createCenterTower() {
 function createFort() {
   const fort = new THREE.Group();
 
-  const gndsize = 8*x;
+  const gndsize = 20*x;
   const gndGeometry = new THREE.BoxGeometry(gndsize, x/10, gndsize);
   const gndMaterial = new THREE.MeshStandardMaterial({color: 0x555555});
   const gnd = new THREE.Mesh(gndGeometry, gndMaterial);
   fort.add(gnd);
 
   const tower1 = createCornerTower();
-  tower1.position.set(-(3*x), 2.05*x, -(3*x));
+  tower1.position.set(-(5*x), 2.05*x, -(5*x));
   fort.add(tower1);
 
-  const wall1 = createWall();
-  wall1.position.set(0, 1.55*x, 3*x);
-  fort.add(wall1);
+  const insideWall1 = createInsideWall();
+  insideWall1.position.set(0, 1.55*x, 5*x);
+  fort.add(insideWall1);
 
   const tower2 = createCornerTower();
-  tower2.position.set(-(3*x), 2.05*x, (3*x));
+  tower2.position.set(-(5*x), 2.05*x, (5*x));
   fort.add(tower2);
 
-  const wall2 = createWall();
-  wall2.rotation.y = Math.PI/2;
-  wall2.position.set(3*x, 1.55*x, 0);
-  fort.add(wall2);
+  const insideWall2 = createInsideWall();
+  insideWall2.rotation.y = Math.PI/2;
+  insideWall2.position.set(5*x, 1.55*x, 0);
+  fort.add(insideWall2);
 
   const tower3 = createCornerTower();
-  tower3.position.set(3*x, 2.05*x, -(3*x));
+  tower3.position.set(5*x, 2.05*x, -(5*x));
   fort.add(tower3);
 
-  const wall3 = createWall();
-  wall3.position.set(0, 1.55*x, -(3*x));
-  fort.add(wall3);
+  const insideWall3 = createInsideWall();
+  insideWall3.position.set(0, 1.55*x, -(5*x));
+  fort.add(insideWall3);
 
   const tower4 = createCornerTower();
-  tower4.position.set(3*x, 2.05*x, 3*x);
+  tower4.position.set(5*x, 2.05*x, 5*x);
   fort.add(tower4);
 
-  const wall4 = createWall();
-  wall4.rotation.y = Math.PI/2;
-  wall4.position.set(-(3*x), 1.55*x, 0);
-  fort.add(wall4);
+  const insideWall4 = createInsideWall();
+  insideWall4.rotation.y = Math.PI/2;
+  insideWall4.position.set(-(5*x), 1.55*x, 0);
+  fort.add(insideWall4);
 
   const centerTower = createCenterTower();
   centerTower.position.set(0, 3.55*x, 0);
   fort.add( centerTower);
   
+  const outsideWall1 = createOutsideWall();
+  outsideWall1.position.set(-0.25*x, 1.3*x, 9.75*x);
+  fort.add(outsideWall1);
+
+  const outsideWall2 = createOutsideWall();
+  outsideWall2.rotation.y = Math.PI/2;
+  outsideWall2.position.set(9.75*x, 1.3*x, 0.25*x);
+  fort.add(outsideWall2);
+
+  const outsideWall3 = createOutsideWall();
+  outsideWall3.position.set(0.25*x, 1.3*x, -9.75*x);
+  fort.add(outsideWall3);
+
+  const outsideWall4 = createOutsideWall();
+  outsideWall4.rotation.y = Math.PI/2;
+  outsideWall4.position.set(-9.75*x, 1.3*x, -0.25*x);
+  fort.add(outsideWall4);
+
   return fort;
 
 }
